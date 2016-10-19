85c65fb3419bf55c8d60c2b207b681383a6cdb49
Hugo Fragata de Oliveira Torres <hugofragata@ua.pt>
2016-10-19 21:49:24 +0100

    explorar opções na lib cryptography

--- User.py
+++ User.py
@@ -1,9 +1,15 @@
 import os, sys
+import random
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+import string
 
 class User:
 
     def __init__(self, name):
         self.name = name
+
         #TODO: manage keys and log in
 
     def cipher_text(self, text):
@@ -20,3 +26,12 @@ class User:
 
     def login(self, user, password):
         pass
+
+    def gen_key_pair(self):
+        self.private_key = rsa.generate_private_key(65537, 4096, default_backend())
+        self.public_key = self.private_key.public_key()
+
+
+    def gen_client_random(self):
+        return ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(32))
+
