ec8dfc317619c44108bd284d353b85ab7bbd5bec
Gonçalo Grilo <grilogoncalo31@ua.pt>
2016-11-05 12:14:05 +0000

    User changed

--- User.py
+++ User.py
@@ -1,36 +1,13 @@
-import os, sys
-import random
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import serialization
-import string
+
 
 class User:
 
     def __init__(self, name):
         self.name = name
+        self.id = None
+        self.sa_data = None
+        self.connection_state = 0
 
-        #TODO: manage keys and log in
-
-    def cipher_text(self, text):
+    def send(self, text):
         pass
-
-    def sign_text(self, text):
-        pass
-
-    def decipher_text(self, text):
-        pass
-
-    def validate_source(self, user_source, text):
-        pass
-
-    def login(self, user, password):
-        pass
-
-    def gen_key_pair(self):
-        self.private_key = rsa.generate_private_key(65537, 4096, default_backend())
-        self.public_key = self.private_key.public_key()
-
-    def gen_client_random(self):
-        return ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(32))
 
